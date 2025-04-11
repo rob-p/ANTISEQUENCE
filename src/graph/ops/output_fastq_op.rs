613de73c16f2d83a7daa0dd81bc41b4d646f4462
Rob Patro <rob.patro@gmail.com>
2025-04-11 01:47:12 -0400

    new buffer strategy

--- src/graph/ops/output_fastq_op.rs
+++ src/graph/ops/output_fastq_op.rs
@@ -1,25 +1,168 @@
-use std::cell::Cell;
+use rustc_hash::FxHashMap;
+use smallvec::{smallvec, SmallVec};
+use std::borrow::Borrow;
+use std::cell::{Cell, RefCell};
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::fs::File;
+use std::io::Cursor;
 use std::io::{BufWriter, Write};
+use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use thread_local::ThreadLocal;
-
-use rustc_hash::FxHashMap;
 
 use flate2::{write::GzEncoder, Compression};
 
 use crate::graph::*;
 
-const MEGABYTE: usize = 1000000;
+const MEGABYTE: usize = 65536;
+
+pub struct LocalSetBuffer {
+    raw_dat: SmallVec<[Cursor<Vec<u8>>; 8]>,
+    dim: usize,
+}
+
+impl LocalSetBuffer {
+    pub fn new(nb: usize) -> Self {
+        assert!(nb > 0);
+        Self {
+            raw_dat: smallvec![Cursor::new(vec![]); nb],
+            dim: nb,
+        }
+    }
+
+    pub fn add_read(&mut self, idx: usize, name: &[u8], seq: &[u8], qual: &[u8]) {
+        let writer = &mut self.raw_dat[idx];
+        writer.write_all(b"@").unwrap();
+        writer.write_all(name).unwrap();
+        writer.write_all(b"\n").unwrap();
+        writer.write_all(seq).unwrap();
+        writer.write_all(b"\n+\n").unwrap();
+        writer.write_all(qual).unwrap();
+        writer.write_all(b"\n").unwrap();
+    }
+
+    pub fn clear(&mut self) {
+        self.raw_dat.iter_mut().for_each(|b| {
+            b.get_mut().clear();
+            b.set_position(0)
+        });
+    }
+
+    pub fn num_bytes(&self) -> usize {
+        self.raw_dat
+            .iter()
+            .fold(0, |acc, x| acc + x.get_ref().len())
+    }
+}
+
+pub struct LocalBuffer {
+    raw_dat: SmallVec<[Vec<u8>; 8]>,
+    lens: SmallVec<[Vec<(usize, usize)>; 8]>,
+    dim: usize,
+}
+
+impl LocalBuffer {
+    pub fn new(nb: usize) -> Self {
+        assert!(nb > 0);
+        Self {
+            raw_dat: smallvec![vec![]; nb],
+            lens: smallvec![vec![]; nb],
+            dim: nb,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.lens[0].len()
+    }
+
+    pub fn num_bytes(&self) -> usize {
+        self.raw_dat.iter().fold(0, |acc, x| acc + x.len())
+    }
+
+    pub fn clear(&mut self) {
+        for i in 0..self.dim {
+            self.raw_dat[i].clear();
+            self.lens[i].clear();
+        }
+    }
+
+    pub fn add_read(&mut self, idx: usize, name: &[u8], seq: &[u8], qual: &[u8]) {
+        let ln = name.len();
+        let ls = seq.len();
+        let mut rd = &mut self.raw_dat[idx];
+        rd.extend_from_slice(name);
+        rd.extend_from_slice(seq);
+        rd.extend_from_slice(qual);
+        self.lens[idx].push((ln, ls))
+    }
+
+    pub fn iter(&self, dim_idx: usize) -> LocalBufferIter {
+        LocalBufferIter {
+            lb: &self,
+            dim_idx,
+            idx: 0,
+            offset: 0,
+        }
+    }
+}
+
+struct LocalBufferIter<'a> {
+    pub lb: &'a LocalBuffer,
+    pub dim_idx: usize,
+    pub idx: usize,
+    pub offset: usize,
+}
+
+impl<'a> Iterator for LocalBufferIter<'a> {
+    type Item = (&'a [u8], &'a [u8], &'a [u8]);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx < self.lb.len() {
+            let (nl, sl) = self.lb.lens[self.dim_idx][self.idx];
+            let res = (
+                &self.lb.raw_dat[self.dim_idx][self.offset..self.offset + nl],
+                &self.lb.raw_dat[self.dim_idx][self.offset + nl..self.offset + nl + sl],
+                &self.lb.raw_dat[self.dim_idx][self.offset + nl + sl..self.offset + nl + sl + sl],
+            );
+            self.offset += nl + sl + sl;
+            self.idx += 1;
+            return Some(res);
+        } else {
+            return None;
+        }
+    }
+}
+
+fn init_writers(file_names: &[PathBuf]) -> std::io::Result<Vec<Box<dyn Write + Send>>> {
+    let mut file_writers = vec![]; //self.file_writers.lock().unwrap();
+
+    for file_path in file_names {
+        // need to create the output file
+        if let Some(parent) = std::path::Path::new(file_path).parent() {
+            std::fs::create_dir_all(parent)?;
+        }
+
+        let writer: Box<dyn Write + Send> = if file_path.ends_with(".gz") {
+            Box::new(BufWriter::new(GzEncoder::new(
+                File::create(file_path)?,
+                Compression::default(),
+            )))
+        } else {
+            Box::new(BufWriter::new(File::create(file_path)?))
+        };
+
+        file_writers.push(writer);
+    }
+    Ok(file_writers)
+}
 
 pub struct OutputFastqFileOp {
     required_names: Vec<LabelOrAttr>,
     file_exprs: Vec<Expr>,
-    file_writers: Mutex<FxHashMap<Vec<u8>, Arc<Mutex<dyn Write + Send>>>>,
-    buffer: ThreadLocal<Cell<HashMap<Vec<u8>, Vec<(Vec<u8>, Vec<u8>, Vec<u8>)>>>>,
-    buffer_size: ThreadLocal<Cell<usize>>,
+    file_paths: Vec<std::path::PathBuf>,
+    file_writers: Mutex<Vec<Box<dyn Write + Send>>>, //FxHashMap<Vec<u8>, Arc<Mutex<dyn Write + Send>>>>,
+    buffer: ThreadLocal<RefCell<LocalSetBuffer>>,
 }
 
 impl OutputFastqFileOp {
@@ -28,34 +171,57 @@ impl OutputFastqFileOp {
     /// Output reads (read 1 only) to a file whose path is specified by an expression.
     pub fn from_file(file_expr: impl Into<Expr>) -> Self {
         let file_expr = file_expr.into();
+        let read = Read::new();
+        let file_name = file_expr.eval_bytes(&read, false);
+        let file_path = PathBuf::from(std::str::from_utf8(file_name.unwrap().borrow()).unwrap());
 
+        let b = LocalSetBuffer::new(1);
+        let c = RefCell::new(b);
+        let t = ThreadLocal::new();
+        t.get_or(|| c);
+
+        let file_paths = vec![file_path];
+        let fwriters = Mutex::new(init_writers(&file_paths).expect("couldn't initialize writers"));
         Self {
             required_names: file_expr.required_names(),
             file_exprs: vec![file_expr],
-            file_writers: Mutex::new(FxHashMap::default()),
-            buffer: ThreadLocal::<Cell<HashMap<Vec<u8>, Vec<(Vec<u8>, Vec<u8>, Vec<u8>)>>>>::new(),
-            buffer_size: ThreadLocal::<Cell<usize>>::new(),
+            file_paths,
+            file_writers: fwriters,
+            buffer: t,
         }
     }
 
     /// Output reads to separate files whose paths are specified by expressions.
     pub fn from_files<E: Into<Expr>>(file_exprs: impl IntoIterator<Item = E>) -> Self {
         let file_exprs = file_exprs.into_iter().map(|e| e.into()).collect::<Vec<_>>();
+
+        let read = Read::new();
+        let file_paths = file_exprs
+            .iter()
+            .map(|e| {
+                let fname = e.eval_bytes(&read, false);
+                let fstr = String::from(std::str::from_utf8(fname.unwrap().borrow()).unwrap());
+                std::path::PathBuf::from(fstr)
+            })
+            .collect::<Vec<_>>();
+
         let required_names = file_exprs
             .iter()
             .flat_map(|e| e.required_names().into_iter())
             .collect::<Vec<_>>();
 
+        let fwriters = Mutex::new(init_writers(&file_paths).expect("couldn't initialize writers"));
         Self {
             required_names,
             file_exprs,
-            file_writers: Mutex::new(FxHashMap::default()),
-            buffer: ThreadLocal::<Cell<HashMap<Vec<u8>, Vec<(Vec<u8>, Vec<u8>, Vec<u8>)>>>>::new(),
-            buffer_size: ThreadLocal::<Cell<usize>>::new(),
+            file_paths,
+            file_writers: fwriters,
+            buffer: ThreadLocal::<RefCell<LocalSetBuffer>>::new(),
         }
     }
 
     // get the corresponding file writer for each read first so writing to different files can be parallelized
+    /*
     fn get_writer(&self, file_name: &[u8]) -> std::io::Result<Arc<Mutex<dyn Write + Send>>> {
         use std::collections::hash_map::Entry::*;
         let mut file_writers = self.file_writers.lock().unwrap();
@@ -83,59 +249,34 @@ impl OutputFastqFileOp {
             }
         }
     }
+    */
 }
 
 impl<T: Trace> GraphNode<T> for OutputFastqFileOp {
     fn run_inner(&self, read: Read) -> Result<(Option<Read>, bool)> {
-        for (i, file_expr) in self.file_exprs.iter().enumerate() {
-            let file_name = file_expr
-                .eval_bytes(&read, false)
-                .map_err(|e| Error::NameError {
-                    source: e,
-                    read: read.clone(),
-                    context: Self::NAME,
-                })?;
+        let mut local_buffer = self
+            .buffer
+            .get_or(|| RefCell::new(LocalSetBuffer::new(self.file_paths.len())))
+            .borrow_mut();
 
+        for i in 0..self.file_paths.len() {
+            // get the i-th record in the read
             let record = read.to_fastq((i + 1) as _).map_err(|e| Error::NameError {
                 source: e,
                 read: read.clone(),
                 context: Self::NAME,
             })?;
-
-            let tupled_record = (record.0.to_vec(), record.1.to_vec(), record.2.to_vec());
-
-            let mut local_buffer = self.buffer.get_or(|| Cell::new(HashMap::new())).take();
-            let local_buffer_size = self.buffer_size.get_or(|| Cell::new(0));
-
-            match local_buffer.entry(file_name.to_vec()) {
-                Entry::Occupied(e) => {
-                    e.into_mut().push(tupled_record);
-                }
-                Entry::Vacant(e) => {
-                    e.insert(vec![tupled_record]);
-                }
-            };
-
-            local_buffer_size.set(local_buffer_size.get() + record_size(record));
-
-            if local_buffer_size.get() >= MEGABYTE {
-                for fname in local_buffer.keys() {
-                    let locked_writer = self.get_writer(&fname).map_err(|e| Error::FileIo {
-                        file: utf8(&fname),
-                        source: Box::new(e),
-                    })?;
-
-                    let mut writer = locked_writer.lock().unwrap();
-                    for (s, r, c) in local_buffer.get(fname).unwrap() {
-                        write_fastq_record(&mut *writer, (&s, &r, &c));
-                    }
-                }
-
-                local_buffer.clear();
-                local_buffer_size.set(0);
-            }
+            local_buffer.add_read(i, record.0, record.1, record.2);
         }
-
+        // if we exceeded the buffer size, then acquire the lock and write the output
+        if local_buffer.num_bytes() >= MEGABYTE {
+            let mut writer_lock = self.file_writers.lock().unwrap();
+            for i in 0..self.file_paths.len() {
+                local_buffer.raw_dat[i].set_position(0);
+                std::io::copy(&mut local_buffer.raw_dat[i], &mut writer_lock[i]);
+            }
+            local_buffer.clear();
+        }
         Ok((Some(read), false))
     }
 
@@ -148,24 +289,15 @@ impl<T: Trace> GraphNode<T> for OutputFastqFileOp {
     }
 
     fn finish(&self) -> Result<bool> {
-        let mut local_buffer = self.buffer.get_or(|| Cell::new(HashMap::new())).take();
-        let local_buffer_size = self.buffer_size.get_or(|| Cell::new(0));
-
-        for fname in local_buffer.keys() {
-            let locked_writer = self.get_writer(&fname).map_err(|e| Error::FileIo {
-                file: utf8(&fname),
-                source: Box::new(e),
-            })?;
-
-            let mut writer = locked_writer.lock().unwrap();
-            for (s, r, c) in local_buffer.get(fname).unwrap() {
-                write_fastq_record(&mut *writer, (&s, &r, &c));
+        if let Some(rc) = self.buffer.get() {
+            let mut local_buffer = rc.borrow_mut();
+            let mut writer_lock = self.file_writers.lock().unwrap();
+            for i in 0..self.file_paths.len() {
+                local_buffer.raw_dat[i].set_position(0);
+                std::io::copy(&mut local_buffer.raw_dat[i], &mut writer_lock[i]);
             }
+            local_buffer.clear();
         }
-
-        local_buffer.clear();
-        local_buffer_size.set(0);
-
         Ok(true)
     }
 }
